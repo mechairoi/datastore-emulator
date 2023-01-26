985a8f4ec0de7765c58d03940268c9764140d0b3
mechairoi <ttsujikawa@gmail.com>
2023-01-26 19:54:05 +0900

    Support command line options

--- start.sh
+++ start.sh
@@ -19,4 +19,4 @@ if [ -f $INDEX_XML_FILE ]; then
 fi
 
 # Run the datastore
-/cloud-datastore-emulator/cloud_datastore_emulator start --host=0.0.0.0 --port=8282 --store_on_disk=false --consistency=1.0 /datastore
+/cloud-datastore-emulator/cloud_datastore_emulator start --host=0.0.0.0 --port=8282 --store_on_disk=false --consistency=1.0 "$@" /datastore
